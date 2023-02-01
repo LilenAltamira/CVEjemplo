06443a2cc1550113b29e1f49af5a556c335d0c6e
LilenAltamira <122871390+LilenAltamira@users.noreply.github.com>
2023-02-01 21:22:54 +0100

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,13 @@
+const typed = new Typed ('.typed',{
+  strings: ['creativa', 'detallista', 'apasionada','curiosa','metódica',],
+  typeSpeed: 75,
+  startDelay: 300,
+  backSpeed: 75,
+  shuffle: false,
+  backDelay: 1500,
+  loop: true,
+  loopCount: false,
+  showCursor: true,
+  cursorChar: '|',
+  contentType: 'html', 
+} );
